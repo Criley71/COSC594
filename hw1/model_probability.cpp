2eb1313ead0a2dcd59358ae9433f3ea7ed12b57e
Criley71 <connorriley@outlook.com>
2025-08-21 10:58:20 -0400

    markov

--- hw1/model_probability.cpp
+++ hw1/model_probability.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <math.h>
 #include <array>
+#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -27,8 +28,8 @@ int main(int argc, char *argv[])
   }
   // cout << sequence;
   fin.close();
-  
-  //begin multinomial model calculations
+
+  // begin multinomial model calculations
   array<double, 256> nucleotides;
   nucleotides['A'] = 0;
   nucleotides['T'] = 0;
@@ -62,42 +63,127 @@ int main(int argc, char *argv[])
   line = "";
   string neander_sequence = "";
   skip = true;
-  while(getline(fin, line)){
-    if(skip){
+  while (getline(fin, line))
+  {
+    if (skip)
+    {
       skip = false;
       continue;
     }
     neander_sequence += line;
   }
   fin.close();
-  //cout << neander_sequence << "\n";
+  // cout << neander_sequence << "\n";
   double prob = 0;
-  for(int i = 0; i < neander_sequence.length(); i++){
-    switch(neander_sequence[i]){
-      case 'A': 
-        prob += log(a_prob); 
-        break;
-      case 'C':
-        prob += log(c_prob);
-        break;
-      case 'G':
-        prob += log(g_prob);
-        break;
-      case 'T':
-        prob += log(t_prob);
-        break;
-      default:
-        prob += log(a_prob); //if nucletide is N then add A's probability
-        break;
+  for (int i = 0; i < neander_sequence.length(); i++)
+  {
+    switch (neander_sequence[i])
+    {
+    case 'A':
+      prob += log(a_prob);
+      break;
+    case 'C':
+      prob += log(c_prob);
+      break;
+    case 'G':
+      prob += log(g_prob);
+      break;
+    case 'T':
+      prob += log(t_prob);
+      break;
+    default:
+      prob += log(a_prob); // if nucletide is N then add A's probability
+      break;
     }
-
   }
-  cout << prob << "\n"; //expected is -452.662
-  //end multinomial model 
-  //begin 3rd order markov model
+  cout << prob << "\n"; // expected is -452.662
+  // end multinomial model
+  // begin 3rd order markov model
   int first = 0;
   int second = 1;
   int third = 2;
   int curr = 3;
+  double markov_array[4][4][4][4] = {0};
+  string markov_sequence = "";
+  for (int i = 0; i < sequence.length(); i++)
+  {
+    switch (sequence[i])
+    {
+    case 'A':
+      markov_sequence += 'A';
+      break;
+    case 'C':
+      markov_sequence += 'B';
+      break;
+    case 'D':
+      markov_sequence += 'C';
+      break;
+    case 'T':
+      markov_sequence += 'D';
+      break;
+    default:
+      markov_sequence += 'A';
+      break;
+    }
+  }
+//  cout << markov_sequence << "\n";
+  while(curr < markov_sequence.length()){
+    markov_array[markov_sequence[first] - 'A'][markov_sequence[second] - 'A'][markov_sequence[third] - 'A'][markov_sequence[curr]-'A'] += 1;
+    first = second;
+    second = third;
+    third = curr;
+    curr+=1;
+  }
+  
+  //int mar_sum = 0;
+  double mar_prob_array[4][4][4][4] = {0};
+  for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 4; j++){
+      for(int k = 0; k < 4; k++){
+        for(int m = 0; m < 4; m++){
+          int order_sum = markov_array[i][j][k][0] + markov_array[i][j][k][1] + markov_array[i][j][k][2] + markov_array[i][j][k][3];
+          if( order_sum == 0)break;
+          //cout << "order_sum: " << order_sum << "\n";
+          //cout << "markov_array: " << markov_array[i][j][k][m] << "\n";
+          mar_prob_array[i][j][k][m] = markov_array[i][j][k][m] / order_sum;
+          //cout <<  "mar_prob_array: "<<mar_prob_array[i][j][k][m] << "\n";
+
+          //mar_sum += markov_array[i][j][k][m];
+        }
+      }
+    }
+  }
+  double markov_prob = 0;
+  string markov_neander_seq = "";
+  for(int i = 0; i < neander_sequence.length(); i++){
+    switch (neander_sequence[i])
+    {
+    case 'A':
+      markov_neander_seq += 'A';
+      break;
+    case 'C':
+      markov_neander_seq += 'B';
+      break;
+    case 'D':
+      markov_neander_seq += 'C';
+      break;
+    case 'T':
+      markov_neander_seq += 'D';
+      break;
+    default:
+      markov_neander_seq += 'A';
+      break;
+  }
+  first = 0;
+  second = 1;
+  third = 2;
+  curr = 3;
+  while(curr < markov_neander_seq.length()){
+    
+  }
+  //cout << markov_sequence.length() << "\n";
+  //cout << mar_sum << "\n";
+  
+
 
 }
