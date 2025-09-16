7db43dd9906cb5c453738878abd7126af4c359e6
Criley71 <connorriley@outlook.com>
2025-09-16 16:17:17 -0400

    hw3

--- hw3_criley16/forward.cpp
+++ hw3_criley16/forward.cpp
@@ -21,8 +21,14 @@ int main() {
   int num_rolls = dice_rolls.size();
   vector<pair<double, bool>> fair_sums;  // first val is sum, bool is if it was the greater value at index
   vector<pair<double, bool>> load_sums;  // first val is sum, bool is if it was the greater value at index
+  double swap_chance = .05;
+  double swap_stay = .95;
+  double sixth = 1/6;
+  vector<double> loaded_chances = {.1, .1, .1, .1, .1, .5};
+  vector<double> fair_chances = {sixth, sixth, sixth, sixth, sixth, sixth};
   fair_sums.resize(dice_rolls.size() + 1);
   load_sums.resize(dice_rolls.size() + 1);
   fair_sums[0] = make_pair(1, true);
   load_sums[0] = make_pair(0, false);
+
 }
