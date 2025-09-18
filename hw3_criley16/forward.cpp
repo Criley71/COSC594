912104e2f66868b4d42a3bca555142d653570f0a
Criley71 <connorriley@outlook.com>
2025-09-18 18:59:16 -0400

    viterbi

--- hw3_criley16/forward.cpp
+++ hw3_criley16/forward.cpp
@@ -1,11 +1,11 @@
-#include <time.h>
-#include <math.h>
 #include <fstream>
 #include <iostream>
+#include <math.h>
+#include <time.h>
 #include <vector>
 using namespace std;
 
-void forward(string dice_rolls);
+void forward(string file);
 
 int main() {
   forward("casino.benchmark1.txt");
@@ -13,8 +13,8 @@ int main() {
   return 0;
 }
 
-void forward(string file){
-ifstream fin(file);
+void forward(string file) {
+  ifstream fin(file);
   if (!fin.good()) {
     perror("error");
     exit(1);
@@ -33,16 +33,23 @@ ifstream fin(file);
   fair[0] = 1;
   load[0] = 0;
   double sixth = 1.0 / 6.0;
+  string check = "";
   vector<double> fair_chances = {sixth, sixth, sixth, sixth, sixth, sixth};
   vector<double> load_chances = {.1, .1, .1, .1, .1, .5};
   for (int i = 1; i <= num_rolls; i++) {
     int roll = dice_rolls[i - 1] - '0';
-    double fair_roll_chance = fair_chances[roll-1];
-    double load_roll_chance = load_chances[roll-1];
+    double fair_roll_chance = fair_chances[roll - 1];
+    double load_roll_chance = load_chances[roll - 1];
 
-    fair[i] = (fair[i-1]*.95 + load[i-1]*.1) * fair_roll_chance;
-    load[i] = (fair[i-1]*.05 + load[i-1]*.9) * load_roll_chance;
+    fair[i] = (fair[i - 1] * .95 + load[i - 1] * .1) * fair_roll_chance;
+    load[i] = (fair[i - 1] * .05 + load[i - 1] * .9) * load_roll_chance;
+    if(fair[i] > load[i]){
+      check += 'F';
+    }else{
+      check += 'L';
+    }
   }
   double probability = fair[num_rolls] + load[num_rolls];
   cout << "Probability of sequence: " << probability << "\n";
+  cout << check << "\n";
 }
---
+++ hw3_criley16/viterbi.cpp
@@ -0,0 +1,80 @@
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <vector>
+using namespace std;
+
+void viterbi(string file, string out) {
+  ifstream fin(file);
+  if (!fin.good()) {
+    perror("error");
+    exit(1);
+  }
+  string buffer = "";
+  string dice_rolls;
+  while (getline(fin, buffer)) {
+    dice_rolls += buffer;
+  }
+  fin.close();
+  int num_rolls = dice_rolls.size();
+  vector<pair<double, string>> fair;   // double is the calculated value.
+  vector<pair<double, string>> loaded; // string will say if the previous was fair or loaded
+  fair.resize(1 + num_rolls);
+  loaded.resize(1 + num_rolls);
+  fair[0] = {1.0, "start"};
+  loaded[0] = {0.0, "start"};
+  double sixth = 1.0 / 6.0;
+  vector<double> fair_chances = {sixth, sixth, sixth, sixth, sixth, sixth};
+  vector<double> loaded_chances = {.1, .1, .1, .1, .1, .5};
+  for (int i = 1; i <= num_rolls; i++) {
+    int roll = dice_rolls[i - 1] - '0';
+    double fair_roll_chance = fair_chances[roll - 1];
+    double loaded_roll_chance = loaded_chances[roll - 1];
+
+    double fair_to_fair = fair[i - 1].first * .95 * fair_roll_chance;
+    double loaded_to_fair = loaded[i - 1].first * .1 * fair_roll_chance;
+    if (fair_to_fair > loaded_to_fair) {
+      fair[i] = {fair_to_fair, "fair"};
+    } else {
+      fair[i] = {loaded_to_fair, "load"};
+    }
+
+    double loaded_to_loaded = loaded[i - 1].first * .9 * loaded_roll_chance;
+    double fair_to_loaded = fair[i - 1].first * .05 * loaded_roll_chance;
+    if (loaded_to_loaded > fair_to_loaded) {
+      loaded[i] = {loaded_to_loaded, "load"};
+    } else {
+      loaded[i] = {fair_to_loaded, "fair"};
+    }
+  }
+  string trace_back = "";
+  string state = "";
+  if (loaded[num_rolls].first > fair[num_rolls].first) {
+    state = "load";
+  } else {
+    state = "fair";
+  }
+  for (int i = num_rolls; i > 0; i--) {
+    if (state == "fair") {
+      trace_back = 'F' + trace_back;
+      state = fair[i].second;
+    } else {
+      trace_back = 'L' + trace_back;
+      state = loaded[i].second;
+    }
+  }
+  ofstream fout(out);
+  if (!fout.good()) {
+    perror("Error");
+    return;
+  }
+  fout << dice_rolls << "\n"
+       << trace_back;
+}
+
+int main() {
+  viterbi("casino.benchmark1.txt", "viterbi.1.txt");
+  viterbi("casino.benchmark2.txt", "viterbi.2.txt");
+  return 0;
+}
